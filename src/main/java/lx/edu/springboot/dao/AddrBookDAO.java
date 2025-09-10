534d22eb0abc62cd40d92bb5b1ccb88e5537eb7d
seojikyeon <seojikyeon@gmail.com>
2025-09-10 12:24:50 +0900

    feat/edit

--- src/main/java/lx/edu/springboot/dao/AddrBookDAO.java
+++ src/main/java/lx/edu/springboot/dao/AddrBookDAO.java
@@ -44,44 +44,11 @@ public class AddrBookDAO {
 	}
 	
 	
-	
-	/*
-	 * public boolean deleteDB(int abId) throws Exception {
-	 * 
-	 * boolean result = false; SqlSession session = SessionUtil.getSession(); int
-	 * deletedAmout = session.delete("deleteDB", abId); session.commit(); if
-	 * (deletedAmout > 0) result = true;
-	 * 
-	 * return result; }
-	 */
-		/*
-		 * System.out.println("deleteDB 함수 호출" + abId); boolean message = false;
-		 * Connection con = getConnection(); String sql =
-		 * "delete from addrbook where ab_id =?"; PreparedStatement psmt =
-		 * con.prepareStatement(sql); psmt.setInt(1, abId); psmt.execute(); con.close();
-		 * 
-		 * 
-		 * message = true;
-		 */
-
-
-	/*
-	 * connection = getConnection(); PreparedStatement pstmt =
-	 * connection.prepareStatement(sqlInsertDB); boolean result= false; try {
-	 * pstmt.setString(1, ab.getAbName());
-	 * //AB_ID,AB_NAME,ab_email,ab_comdept,ab_birth,ab_tel,ab_memo) "
-	 * pstmt.setString(2, ab.getAbEmail()); pstmt.setString(3, ab.getAbTel());
-	 * pstmt.setString(4, ab.getAbBirth()); pstmt.setString(5, ab.getAbComdept());
-	 * pstmt.setString(6, ab.getAbMemo()); result= pstmt.execute(); } catch
-	 * (Exception e) { e.printStackTrace(); }
-	 */
-
-	
-	/*
-	 * public AddrBookVO getDB(int abId) throws Exception{
-	 *  	return session.selectOne("getDB", abID);
-	 *  	} 
-	 */
+ /**
+  * 
+  * branch park
+  * 
+  */
 
 	
 
